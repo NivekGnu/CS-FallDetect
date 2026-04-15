5ab2b8529bb69201a325452590dc235fa083835f
Kevin Ung <ungkevin16@gmail.com>
2026-04-14 23:44:11 -0700

    implement while looping logic

--- server/main.py
+++ server/main.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from camera import Camera
+import cv2
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+camera = Camera()
+
+def generate_frames():
+    while True:
+        frame = camera.process()
+        if frame is None:
+            break
+
+        _, buffer = cv2.imencode('.jpg', frame)
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
 @app.get("/")
 async def root():
@@ -8,8 +31,10 @@ async def root():
 
 @app.get("/video_feed")
 async def video_feed():
-    return {"message": "Video feed"}
+    return StreamingResponse(generate_frames(),
+                             media_type="multipart/x-mixed-replace; boundary=frame")
 
 @app.get("/status")
 async def status():
-    return {"message": "Status"}
+    return {"fall_detected": camera.fall_active,
+            "last_alert_time": camera.last_alert_time}
